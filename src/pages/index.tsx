80d9ec76a043127f508800c9f42431ee95e75d6d
Emil <norito235@gmail.com>
2023-06-23 14:00:48 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { useSession, signIn, signOut, getSession } from "next-auth/react"
+import { NextPageContext } from "next"
+
+export default function Home() {
+  const { data:session } = useSession()
+  console.log(session)
+  return (
+    <>
+    <h1 className="text-gray-500 text-4xl bg-white">
+      {session?.user?.name}
+    </h1>
+    {session ? (
+      <button onClick={() => signOut()} className='bg-blue-400 p-1'>Sign out</button>
+    ) :
+    <button onClick={() => signIn()} className='bg-blue-400 p-1'>Sign in</button>
+    }
+    </>
+  )
+}
+
+export async function getServerSideProps( ctx: NextPageContext) {
+  const session = await getSession(ctx)
+  console.log(session) 
+  return {
+    props: {
+      session,
+    },
+  }
+}
---
+++ src/utils/connectDb.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+if (!process.env.DATABASE_URL){
+    throw new Error('Please add the DATABASE_URL environment variable')
+}
+
+const DATABASE_URL : string = process.env.DATABASE_URL;
+
+let globalWithMongoose = global as typeof globalThis & {
+    mongoose: any;
+};
+
+let cached = globalWithMongoose.mongoose;
+
+if (!cached) {
+    cached = globalWithMongoose.mongoose = { connection: null, promise: null};
+}
+
+async function connectDb() {
+    if (cached.connection){
+        return cached.connection
+    }
+    if (!cached.promise){
+        const options = {
+            bufferCommands: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+    cached.promise = mongoose.connect(DATABASE_URL, options).then((mongoose) => {
+        console.log('Connected to database');
+        return mongoose;
+    }).catch((err) => {
+        console.log(err as Error)
+    })
+    }
+    cached.connection = await cached.promise;
+ 
+    return cached.connection;
+}
+
+export default connectDb;
