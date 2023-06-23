971125ebc2f3c6e28494629fa757847a7e663c5d
Emil <norito235@gmail.com>
2023-06-23 14:37:34 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,46 @@
-import { useSession, signIn, signOut, getSession } from "next-auth/react"
-import { NextPageContext } from "next"
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+import Image from "next/image";
 
 export default function Home() {
-  const { data:session } = useSession()
-  return (
-    <>
-    <h1 className="text-gray-500 text-4xl bg-white">
-      {session?.user?.name}
-    </h1>
-    {session ? (
-      <button onClick={() => signOut()} className='bg-blue-400 p-1'>Sign out</button>
-    ) :
-    <button onClick={() => signIn()} className='bg-blue-400 p-1'>Sign in</button>
-    }
-    </>
-  )
+   const { data: session } = useSession();
+   let imgURL = session?.user?.image;
+   return (
+      <>
+         <h1 className="text-gray-500 text-4xl bg-white">
+            {session?.user?.name}
+         </h1>
+         {session ? (
+            <Image
+               src={imgURL}
+               width={128}
+               height={128}
+               alt=""
+               className="rounded-full"
+            />
+         ) : (
+            <></>
+         )}
+
+         {session ? (
+            <button onClick={() => signOut()} className="bg-blue-400 p-1">
+               Sign out
+            </button>
+         ) : (
+            <button onClick={() => signIn()} className="bg-blue-400 p-1">
+               Sign in
+            </button>
+         )}
+      </>
+   );
 }
 
-export async function getServerSideProps( ctx: NextPageContext) {
-  const session = await getSession(ctx)
-  console.log(session) 
-  return {
-    props: {
-      session,
-    },
-  }
+export async function getServerSideProps(ctx: NextPageContext) {
+   const session = await getSession(ctx);
+   console.log(session);
+   return {
+      props: {
+         session,
+      },
+   };
 }
